bffe91c7c7a15d4d34e38e8c83377c297aabe221
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-01-01 06:54:55 -0500

    featured added

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 let screen = document.getElementById("screen");
 let calculator = document.getElementsByClassName("calculator")[0];
 let buttons = calculator.querySelectorAll("button");
+for (let i = 0; i < buttons.length; i++) {
+    let clickedButton = buttons[i].addEventListener("click", displayScreen);
+}
+function displayScreen(e) {
+    if (e.target.innerText === "Clear"){
+        screen.innerText = "";
+    } else if (e.target.innerText === "="){
+        try {
+            screen.innerText = eval(screen.innerText);
+        } catch {
+            screen.innerText = "ERROR..."
+        }
+        screen.innerText = eval(screen.innerText);
+    }
+    
+    else {
+        screen.innerText += e.target.innerText;
+    }
+}
